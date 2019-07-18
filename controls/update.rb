4a025f9c425bc55fac2f2b1440dcc90533c6b81c
Ressl Robert <r.ressl@safematix.com>
2019-07-18 23:02:44 +0200

    :pencil: finalize update control

--- controls/update.rb
+++ controls/update.rb
@@ -1,15 +1,17 @@
-title "update"
+title 'update section'
 
 control 'update-1.1' do
   impact 1.0
   title 'CIS: Install Updates, Patches and Additional Security Software'
   desc '1.1 Verify all Apple provided software is current'
   describe command('softwareupdate -l') do
-    its('stderr') { should include "No new software available" }
+    its('stderr') { should include 'No new software available' }
   end
-  #describe command('defaults read /Library/Preferences/com.apple.SoftwareUpdate LastSuccessfulDate') do
-  #  its('::Time.parse(stdout) - ::Time.new') { should be < 3 }
-  #end
+  lastsuccessfuldate = Time.parse(command('defaults read /Library/Preferences/com.apple.SoftwareUpdate LastSuccessfulDate').stdout)
+  diffdays = (Time.new - lastsuccessfuldate) / 60 / 60 / 24
+  describe diffdays do
+    it { should be <= 30 }
+  end
 end
 
 control 'update-1.2' do
@@ -26,6 +28,27 @@ control 'update-1.3' do
   title 'CIS: Install Updates, Patches and Additional Security Software'
   desc '1.3 Enable app update installs'
   describe command('defaults read /Library/Preferences/com.apple.commerce AutoUpdate') do
+    its('stdout.to_i') { should be 1 }
+  end
+end
+
+control 'update-1.4' do
+  impact 1.0
+  title 'CIS: Install Updates, Patches and Additional Security Software'
+  desc '1.4 Enable system data files and security update installs'
+  describe command('defaults read /Library/Preferences/com.apple.SoftwareUpdate ConfigDataInstall') do
+    its('stdout.to_i') { should be 1 }
+  end
+  describe command('defaults read /Library/Preferences/com.apple.SoftwareUpdate CriticalUpdateInstall') do
+    its('stdout.to_i') { should be 1 }
+  end
+end
+
+control 'update-1.5' do
+  impact 1.0
+  title 'CIS: Install Updates, Patches and Additional Security Software'
+  desc '1.5 Enable macOS update installs'
+  describe command('defaults read /Library/Preferences/com.apple.commerce AutoUpdateRestartRequired') do
     its('stdout.to_i') { should be 1 }
   end
 end
